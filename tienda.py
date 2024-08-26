a3431fe97319f595ee2f739871a2f954aae61361
Fernando <fbanarescarrasco@gmail.com>
2024-08-26 11:42:08 -0400

    Se saca listar_productos de la clase restaurante ya que no es necesaria

--- tienda.py
+++ tienda.py
@@ -45,18 +45,12 @@ class Restaurante(Tienda):
     def ingresar_producto(self, nombre: str, precio: int, stock: int = 0):
         Tienda.ingresar_producto(self, nombre, precio, 0)
 
-    def listar_productos(self):
-        listado = ""
-        for producto in self._productos:
-            listado += f"{producto.nombre} - ${producto.precio}\n"
-        return listado
 
     def realizar_venta(self, nombre_producto: str, cantidad: int):
         for producto in self._productos:
             if producto.nombre == nombre_producto:
                 return f"Venta realizada: {cantidad} {nombre_producto}(s)"
         return "Producto no disponible"
-
 
 class Supermercado(Tienda):
     def listar_productos(self):
